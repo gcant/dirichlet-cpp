142034b147e2bf722981d03e1caf714a30797962
George Cantwell <41652900+gcant@users.noreply.github.com>
2019-10-20 11:25:22 -0400

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "dirichlet.h"
+
+
+int main() {
+
+	// initialize rng
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	
+	// Dirichlet distribution using mt19937 rng
+	dirichlet_distribution<std::mt19937> d({1, 2, 3});
+
+	// print 10 random draws
+	for (int i=0; i<10; ++i){
+		for (double x : d(gen)) std::cout << x << " ";
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
